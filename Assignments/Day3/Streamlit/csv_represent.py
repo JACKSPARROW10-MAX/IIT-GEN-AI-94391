9626cb9c0fa97faa7b2e7037c20162169ddeb2da
Prathamesh Salokhe <prathameshsalokhe901@gmail.com>
2025-12-13 10:05:47 +0530

    day 3 first commit

---
+++ Assignments/Day3/Streamlit/csv_represent.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+import pandasql as ps
+st.title("CSV Explorer")
+
+# upload a CSV file
+data_file = st.file_uploader("Upload a CSV file", type=["csv"])
+# load it as dataframe
+if data_file:
+    df = pd.read_csv(data_file)
+    # display the dataframe
+    st.dataframe(df)
+    empid=st.text_input("Enter your empid :")
+    query=f"SELECT * FROM data WHERE empno = '{empid}'"
+    result=ps.sqldf(query,{"data":df})
+    st.dataframe(result)
